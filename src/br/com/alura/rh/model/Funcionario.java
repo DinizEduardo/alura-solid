63e90c46c7d4842547a5899974ebd985399cfcc8
Eduardo Diniz <dinizdudi@gmail.com>
2022-12-12 20:37:40 -0300

    feat: extraindo regra de negocio de reajuste para um service. aumentando a coesão das classes

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -1,9 +1,6 @@
 package br.com.alura.rh.model;
 
-import br.com.alura.rh.ValidacaoException;
-
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
 
 public class Funcionario {
@@ -21,12 +18,8 @@ public class Funcionario {
         this.salario = salario;
     }
 
-    public void reajustarSalario(BigDecimal aumento) {
-        BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-        if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-            throw new ValidacaoException("Reajuste não pode ser superior a 40% do salario.");
-        }
-        this.salario = this.salario.add(aumento);
+    public void atualizarSalario(BigDecimal novoSalario) {
+        this.salario = novoSalario;
         this.dataUltimoReajuste = LocalDate.now();
     }
 
